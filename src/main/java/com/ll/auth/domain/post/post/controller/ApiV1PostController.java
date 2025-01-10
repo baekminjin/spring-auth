8dbf6de84a78d5d140b014cbd054658d4fb71a23
baekminjin <bhs3392@naver.com>
2025-01-10 14:30:36 +0900

    unique Password2

--- src/main/java/com/ll/auth/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/auth/domain/post/post/controller/ApiV1PostController.java
@@ -17,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -28,14 +29,11 @@ public class ApiV1PostController {
 
     private Member checkAuthentication() {
         String credentials = request.getHeader("Authorization");
-        credentials = credentials.substring("Bearer ".length()); //포스트맨 관례
-        String[] credentialsBits = credentials.split("/", 2);
-        long actorId = Long.parseLong(credentialsBits[0]);
-        String actorPassword = credentialsBits[1];
-        Member actor = memberService.findById(actorId).get();
-        if (!actor.getPassword2().equals(actorPassword))
+        String password2 = credentials.substring("Bearer ".length()); //포스트맨 관례
+        Optional<Member> opActor = memberService.findByPassword2(password2);
+        if (opActor.isEmpty())
             throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
-        return actor;
+        return opActor.get();
     }
 
     @GetMapping
